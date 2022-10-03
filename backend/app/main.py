fc874ea81d7d28e94dd2c16fe64be0fb81890340
Zainuddin Siddiqi <zainuddin1@live.co.uk>
2022-10-03 20:02:15 +0100

    move settings to main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,28 @@
 import pusher
 
-from functools import lru_cache
-from fastapi import Depends, FastAPI
+from pydantic import BaseSettings
+from pathlib import Path
+from fastapi import FastAPI
 
-from config import settings
+# settings
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+SQLALCHEMY_DATABASE_URL = "sqlite:///../pizza_rank.db"
 
 
-app = FastAPI()
+class Settings(BaseSettings):
+    app_name: str = "Pizza Rank API"
+    pusher_app_id: str
+    pusher_key: str
+    pusher_secret: str
+    pusher_cluster: str
 
+    class Config:
+        env_file = BASE_DIR / ".env"
+
+
+settings = Settings()
+
+# pusher
 pusher_client = pusher.Pusher(
     app_id=settings.pusher_app_id,
     key=settings.pusher_key,
@@ -15,6 +30,9 @@ pusher_client = pusher.Pusher(
     cluster=settings.pusher_cluster,
     ssl=True,
 )
+
+# fastapi
+app = FastAPI()
 
 
 @app.get("/")
